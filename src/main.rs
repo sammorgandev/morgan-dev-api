b419c57ebd0c9d9d7e0edc788e407f87ca8d2494
sammorgandev <sam@morgan.dev>
2024-05-11 01:50:46 +0000

    working axum server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use std::format;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route(
+            "/hello/:name",
+            get(|params: axum::extract::Path<(String,)>| async move {
+                format!("Hello, {}", (params.0).0)
+            }),
+        );
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
