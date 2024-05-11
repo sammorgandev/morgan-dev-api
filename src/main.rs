626344c9f23431cea0b80ac68ba9e964c83a237c
sammorgandev <sam@morgan.dev>
2024-05-11 02:33:38 +0000

    additional functions

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 //IMPORTS
 //axum is the http server framework
 use axum::{
+    body::Bytes,
     extract::{Json, Path, Query},
+    http::StatusCode,
     routing::get,
     Router,
 };
@@ -9,9 +11,11 @@ use axum::{
 use serde_json::{json, Value};
 
 //standard library
-use std::{collections::HashMap, format};
+use std::{collections::HashMap, format, string};
 
-//UTILITY FUNCTIONS
+//UTILITY & HANDLER FUNCTIONS
+
+//UTILITIES
 //"Path" give you the path parameters and deserializes them into the type you specify
 async fn _path(Path(_user_id): Path<u32>) {}
 
@@ -21,6 +25,7 @@ async fn _query(Query(_params): Query<HashMap<String, String>>) {}
 //buffer the request body and deserialize it as JSON into a serde_json::Value. 'Json' supports any type that implements Deserialize
 async fn _json(Json(_payload): Json<serde_json::Value>) {}
 
+//HANDLERS
 // `&'static str` becomes a `200 OK` with `content-type: text/plain; charset=utf-8`
 async fn _return_plain_text() -> &'static str {
     "foo"
@@ -31,6 +36,13 @@ async fn _return_json() -> Json<Value> {
     Json(json!({ "data": 42 }))
 }
 
+async fn _echo(body: Bytes) -> Result<String, StatusCode> {
+    if let Ok(string) = String::from_utf8(body.to_vec()) {
+        Ok(string)
+    } else {
+        Err(StatusCode::BAD_REQUEST)
+    }
+}
 #[tokio::main]
 async fn main() {
     let app = Router::new()
