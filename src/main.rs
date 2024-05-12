52b54aafc09cfbc5a37cdaafd99a953e43d9b7c8
sammorgandev <sam@morgan.dev>
2024-05-12 01:23:34 +0000

    initial folder architecture

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,14 @@ mod models;
 mod routes;
 use std::sync::Arc;
 //dotenv is used to load environment variables from a .env file
-use db::get_db_client;
+use db::establish_connection;
 use routes::get_routes;
 use tokio_postgres::Error; //tokio is the async runtime and tokio-postgres is the async postgres driver //std is the standard library //db is a module that contains the get_db_client function
 
 //MAIN FUNCTION
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let client = get_db_client()
+    let client = establish_connection()
         .await
         .expect("Failed to connect to database");
 
